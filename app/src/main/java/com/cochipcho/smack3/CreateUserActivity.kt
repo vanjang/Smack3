3583ddb1eae697cebc786cfedf4a215acce3c5a6
Myung Hoon So <joa214@hotmail.com>
2020-06-15 21:04:09 +0100

    avatar created

--- app/src/main/java/com/cochipcho/smack3/CreateUserActivity.kt
+++ app/src/main/java/com/cochipcho/smack3/CreateUserActivity.kt
@@ -1,10 +1,16 @@
 package com.cochipcho.smack3
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlin.random.Random
 
 class CreateUserActivity : AppCompatActivity() {
+
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5, 0.5, 0.5, 1]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,11 +18,34 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
 
+        if (color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar ="dark$avatar"
+        }
+
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createAvatarImageView.setImageResource(resourceId)
     }
 
     fun generateColorButton(view: View) {
+        val random = Random
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
 
+        createAvatarImageView.setBackgroundColor(Color.rgb(r,g,b))
+
+        val savedR = r.toDouble() / 255
+        val savedG = g.toDouble() / 255
+        val savedB = b.toDouble() / 255
+
+        avatarColor = "[$savedR, $savedG, $savedB, 1]"
+        println(avatarColor)
     }
 
     fun createUserClicked(view: View) {
